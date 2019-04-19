3c35b508c1e6642697cce3b96df1cdcd20f0017c
Gregory Wagner <wagner.greg@gmail.com>
2019-04-19 00:17:40 -0400

    Adds kwargs to xzsliceplot

--- src/plotting.jl
+++ src/plotting.jl
@@ -8,10 +8,10 @@ xnodes(ϕ::FaceFieldX) = reshape(ϕ.grid.xF[1:end-1], ϕ.grid.Nx, 1, 1)
 ynodes(ϕ::FaceFieldY) = reshape(ϕ.grid.yF[1:end-1], 1, ϕ.grid.Ny, 1)
 znodes(ϕ::FaceFieldZ) = reshape(ϕ.grid.zF[1:end-1], 1, 1, ϕ.grid.Nz)
 
-xzsliceplot(ϕ::Field, slice=1) = pcolormesh(
+xzsliceplot(ϕ::Field, slice=1; kwargs...) = pcolormesh(
     repeat(dropdims(xnodes(ϕ), dims=2), 1, ϕ.grid.Nz),
     repeat(dropdims(znodes(ϕ), dims=2), ϕ.grid.Nx, 1),
-    ϕ.data[slice, :, :])
+    ϕ.data[slice, :, :], kwargs...)
 
 aspectratio(a, ax=gca(); adjustable="box") = ax.set_aspect(a, adjustable=adjustable)
 makesquare(ax=gca()) = aspectratio(1, ax)
